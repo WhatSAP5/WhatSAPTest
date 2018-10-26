51e7fedeace61427bf7040f1400e387ccd3ca98f
Joy-Shin <joy.shin.2213@gmail.com>
2018-10-26 16:24:37 -0400

    Replaced to Bootstrap 4 and built some index pages

---
+++ WhatSAP/ViewComponents/ActivityViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatSAP.Models;
+
+namespace WhatSAP.ViewComponents
+{
+    [ViewComponent]
+    public class ActivityViewComponent : ViewComponent
+    {
+        private readonly WhatSAPContext _context;
+
+        public ActivityViewComponent(WhatSAPContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var activities = _context.Activity.ToArray();
+            return View(activities);
+        }
+    }
+}
